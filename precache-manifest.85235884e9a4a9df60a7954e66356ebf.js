c1b91213ee09d776d2dff01346e9dc6fcb6792ae
Andrew Denta <andrew@denta.co>
2020-05-17 23:49:18 -0400

    Updates

---
+++ precache-manifest.85235884e9a4a9df60a7954e66356ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1278268614db3d324b604c7bf513afc9",
+    "url": "/dowehaveenoughcovidtestingcapacity/index.html"
+  },
+  {
+    "revision": "7cf6725d319cd230d2c0",
+    "url": "/dowehaveenoughcovidtestingcapacity/static/css/main.005646dc.chunk.css"
+  },
+  {
+    "revision": "eff2cdd514f8af6f553a",
+    "url": "/dowehaveenoughcovidtestingcapacity/static/js/2.7e780301.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/dowehaveenoughcovidtestingcapacity/static/js/2.7e780301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf6725d319cd230d2c0",
+    "url": "/dowehaveenoughcovidtestingcapacity/static/js/main.ae8d3ab5.chunk.js"
+  },
+  {
+    "revision": "7c40aa6c9af6a235d9d2",
+    "url": "/dowehaveenoughcovidtestingcapacity/static/js/runtime-main.3641cad7.js"
+  }
+]);
